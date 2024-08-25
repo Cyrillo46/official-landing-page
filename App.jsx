0101f60ad4778b92841ac556a1ff1fd325eb86d5
Cyrillo46 <yahooisbetter46@gmail.com>
2024-08-25 10:29:33 -0400

    created Navbar component

--- App.jsx
+++ App.jsx
@@ -1,22 +1,13 @@
+import { Navbar } from "./components/Navbar";
+
 const App = () => {
   return (
     <>
-      <div className="h-screen bg-gray-800">
-        <div className="">
-          <div className="bg-rose-300 h-12">
-            <nav>
-              <ul className="flex justify-around">
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
+      <Navbar />
     </>
   );
 };
 
 export default App;
+// import nanoid/react-icons
+// images from https://undraw.co
