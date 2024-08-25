a97517d7dd824a0378338be3e97c61c0823247aa
Cyrillo46 <yahooisbetter46@gmail.com>
2024-08-25 00:09:42 -0400

    background color, sizing and flex covered

--- App.jsx
+++ App.jsx
@@ -1,7 +1,22 @@
-import "./App.css";
-
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <div className="h-screen bg-gray-800">
+        <div className="">
+          <div className="bg-rose-300 h-12">
+            <nav>
+              <ul className="flex justify-around">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
